dbe04500cddc390d200b0ec25fac395098037e86
Roger Waldron <roger.waldron@gmail.com>
2023-06-11 23:30:17 +0200

    feat: API Versioning

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
+	appv1 := app.Group("/api/v1")
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
-
+	appv1.Get("/users", handleUser)
 	app.Listen(":3000")
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "John Wick"})
 }
