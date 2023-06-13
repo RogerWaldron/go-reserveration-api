fe281ecc8c3ef38ffdb066d57cd4c458453519f4
Roger Waldron <roger.waldron@gmail.com>
2023-06-13 15:00:29 +0200

    feat: DB support, GetUsers and GetUserByID

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/RogerWaldron/go-reserveration-api/api"
+	"github.com/RogerWaldron/go-reserveration-api/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const dbURI = "mongodb://localhost:27017"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddress := flag.String("listenAddress", ":3200", "This listen address for API server")
 	flag.Parse()
-
-	app := fiber.New()
+ 
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+	
+	app := fiber.New(config)
 	appv1 := app.Group("/api/v1")
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
-	appv1.Get("/user", api.HandleGetUsers)
-	appv1.Get("/user/:id", api.HandleGetUserByID)
+	appv1.Get("/user", userHandler.HandleGetUsers)
+	appv1.Get("/user/:id", userHandler.HandleGetUserByID)
 
 	app.Listen(*listenAddress) 
 }
