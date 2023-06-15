e32f7bc6f7f13eacacccefcbdce4c973f2c46ca1
Roger Waldron <roger.waldron@gmail.com>
2023-06-15 19:15:24 +0200

    Refactor: DB related consts and types moved

--- db/db.go
+++ db/db.go
@@ -1,3 +1,13 @@
 package db
+
+import "context"
  
-const DBNAME = "reservation"
+const DB_NAME = "reservation"
+const DB_TEST_NAME = DB_NAME + "_test"
+
+type Dropper interface {
+	Drop(context.Context) error
+}
+
+// "any" so can pass in mongoDB ID which is a primitive Object
+type Map map[string]any
